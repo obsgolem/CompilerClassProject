819e2e9e09575c2471c741d496c656ccba867ef6
NATI0N <nati0n@protonmail.com>
2018-05-02 23:06:14 -0400

    Attempt at return expression code

--- compiler/parser/ReturnStatement.java
+++ compiler/parser/ReturnStatement.java
@@ -15,7 +15,23 @@ public class ReturnStatement extends Statement {
 
     // TODO
     public Integer genLLCode(Function func, CompoundStatement scope) throws CodeGenerationException {
-        return -1; //statments return -1, expr regurn regnum with value
+        
+        // Get value of expression
+        Integer res = expr.genLLCode(func, scope);
+
+        // Set value in the retreg
+        Integer reg = func.getNewRegNum();
+        op = new Operation(Operation.OperationType.ASSIGN, func.getCurrBlock());
+        op.setSrcOperand(0, new Operand(Operand.OperandType.INTEGER, res));
+        op.setDestOperand(0, new Operand(Operand.OperandType.MACRO, "RetReg"));
+        func.getCurrBlock().appendOper(op);
+
+        // Return unconditionally to return block
+        Operation ret_branch = new Operation(Operation.OperationType.JMP, func.getCurrBlock());
+        ret_branch.setSrcOperand(0, new Operand(Operand.OperandType.BLOCK, func.getReturnBlock()));
+        func.getCurrBlock().appendOper(ret_branch);
+
+        return -1; //statments should return -1, expr regurn regnum with value
     }
 
     public void printTree(int level) {
